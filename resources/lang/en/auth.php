b299c1697990fd4facc22644b8c4cbd5ef533765
Felipe Braiani <felipe.braianitech@gmail.com>
2017-12-31 14:11:39 -0300

    Campos de mensagens automáticas traduzido para pt-BR. Automatic messages fields translated to pt-BR.'

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Usuário ou senha incorretos. Por favor, verifique as informações e tente novamente.',
+    'throttle' => 'Muitas tentativas de login sem sucesso. Por favor, tente novamente em :seconds segundos.',
 
 ];
